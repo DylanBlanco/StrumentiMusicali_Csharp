d5dd40609e8ed1735c04b7bd7392bf1adbe0c9cd
DylanBlanco <dylanblanco1198@gmail.com>
2025-10-30 00:45:36 +0100

    creato class Tamburo

---
+++ LibreriaClassi/StrumentiMusicali/Tamburo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClassi.StrumentiMusicali
+{
+    public class Tamburo
+    {
+        public string Suono { get; set; }
+
+        public Tamburo() { }
+
+        public Tamburo(string s)
+        {
+            Suono = s;
+        }
+    }
+}
