2b15c32202a7e364b0466fe7a8989b43948229cf
DylanBlanco <dylanblanco1198@gmail.com>
2025-10-30 01:49:42 +0100

    creta interfaccia IStrumentoMusicale

--- LibreriaClassi/StrumentiMusicali/StrumentoMusicale.cs
+++ LibreriaClassi/StrumentiMusicali/StrumentoMusicale.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace LibreriaClassi.StrumentiMusicali
 {
+    // CALSSE ASTRATTA, NON ISTANZIABILE
     public abstract class StrumentoMusicale
     {
+        public abstract string Suona();  // METODO ASTRATTO, OBBLIGA LE CLASSI DERIVATE A IMPLEMENTARLO
+
+        public string Suono { get; set; }  // PROPRIETA' COMUNE A TUTTI GLI STRUMENTI
+
+        public StrumentoMusicale() { }  // COSTRUTTORE DI DEFAULT
+
+        public StrumentoMusicale(string s)
+        {
+            Suono = s;  // COSTRUTTORE CON PARAMETRO
+        }
     }
 }
